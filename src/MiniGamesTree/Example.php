fd54da5312f300aa4bd9e10215d9473adb178642
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-03-07 15:51:27 +0300

    Update Example.php

--- src/MiniGamesTree/Example.php
+++ src/MiniGamesTree/Example.php
@@ -4,8 +4,12 @@ class Example {
 
 
  // That IS Example
-  public function onDeath(PlayerDeathEvent $ev){
-  
+  public function onJoin(PlayerJoinEvent $ev){
+  MiniGamesAPI::getInstance()->add(1,$ev->getPlayer()->getName()); // Add Player 1
+  MiniGamesAPI::getInstance()->getPlayerWithNumber(1)->getInventory()->addItem(Item::get(1));
+  if(MiniGamesAPI::getInstance()->inGame()){
+   MiniGamesAPI::getInstance()->remove(1); // delete Player 1
+  }
   }
 
 }
