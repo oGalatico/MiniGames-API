6c1bee52fbc40aa34d4b1339dae2f3de5a4d0cf4
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-03-07 09:56:46 +0300

    Create Time.php

---
+++ src/MiniGamesTree/Time.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MiniGamesTree;
+
+use pocketmine\Server;
+
+class Time extends Base\ArrayBase{
+    
+    /** @var Server */
+    private $server;
+    
+     /**
+     * @param $s
+     */
+    public function __construct(Server $s){
+        parent::__construct($server);
+        $this->server = $s;
+    }
+    public function setTime($time){
+        $this->server->getPluginManager()->callEvent(new PluginSetTimeEvent($this, $time));
+        return $this->Time = $time + 1;
+    }
+    
+    public function getTimeSeconds(){
+        return $this->Time;
+    }
+    
+}
