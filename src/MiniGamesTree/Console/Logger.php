05cca6c3b547fcb13de4868f97cb442d23e8a767
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-03-07 11:01:27 +0300

    Update Logger.php

--- src/MiniGamesTree/Console/Logger.php
+++ src/MiniGamesTree/Console/Logger.php
@@ -9,7 +9,8 @@ class Logger extends PluginBase {
     const Name = "MiniGamesBase";
     
     public function onEnable() {
-    
+        $base = new MiniGamesTree/Base/ArrayBase;
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new Run\RT($this), $base->speed);
         $this->getLogger()->info(\pocketmine\utils\TextFormat::GREEN.self::Name . " IS Worked");
         
     }
