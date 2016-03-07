770a0c57c78654f796ef84b50a92b2f5ffa12ead
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-03-07 10:58:41 +0300

    Create RT.php

---
+++ src/MiniGamesTree/Console/Run/RT.php
@@ -0,0 +1,24 @@
+<?php
+namespace MiniGamesTree\Console\Run;
+
+use pocketmine\scheduler\PluginTask;
+
+class RT extends PluginTask{
+    public $base;
+    public function __construct(MiniGamesTree\Base\ArrayBase $base){
+        parent::__construct($plugin);
+        $this->base = $base;
+    }
+    public function onRun($tick){
+    $this->TimeM($this->base->move);
+    }
+    
+    public function TimeM($m){
+    if($m == ('-')){
+    $this->base->Time--;
+    }
+    if($m == ('+')){
+    $this->base->Time++;
+    }
+    }
+}
