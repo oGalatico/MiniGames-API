8114f7d4146ad510caba93713fa1a99e2acafa94
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-03-09 14:38:44 +0300

    Update RT.php

--- src/MiniGamesTree/Console/Run/RT.php
+++ src/MiniGamesTree/Console/Run/RT.php
@@ -5,20 +5,20 @@ use pocketmine\scheduler\PluginTask;
 
 class RT extends PluginTask{
     public $base;
-    public function __construct(\MiniGamesTree\Base\ArrayBase $base){
+    public function __construct(\MiniGamesTree\Console\Logger $base){
         parent::__construct($base);
         $this->base = $base;
     }
     public function onRun($tick){
-    $this->TimeM($this->base->move);
+    $this->TimeM($this->base->base->move);
     }
     
     public function TimeM($m){
     if($m == ('-')){
-    $this->base->Time--;
+    $this->base->base->Time--;
     }
     if($m == ('+')){
-    $this->base->Time++;
+    $this->base->base->Time++;
     }
     }
 }
